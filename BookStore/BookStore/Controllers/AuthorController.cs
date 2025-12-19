c27f02de2019dd74793bd9bcd3e1a4b15ad526a4
leandrocassiano <leandrocassiano@outlook.com>
2025-12-19 15:03:13 -0300

    Finalizando Action Filters

--- BookStore/BookStore/Controllers/AuthorController.cs
+++ BookStore/BookStore/Controllers/AuthorController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookStore.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,7 @@ using System.Web.Mvc;
 namespace BookStore.Controllers
 {
     [RoutePrefix("autores")]
+    [LogActionFilter()]
     public class AuthorController : Controller
     {
         [Route("criar")]
